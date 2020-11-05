661a47807119f1290031d86bd25fa8802511c0af
Josh Collins <josh.collins@janeirodigital.com>
2020-11-05 08:05:55 -0500

    another test

--- shapetrees/src/test/java/com/janeirodigital/shapetrees/test/RemoteResourceTests.java
+++ shapetrees/src/test/java/com/janeirodigital/shapetrees/test/RemoteResourceTests.java
@@ -4,18 +4,14 @@ import com.janeirodigital.shapetrees.RemoteResource;
 import com.janeirodigital.shapetrees.ShapeTreeException;
 import com.janeirodigital.shapetrees.test.fixtures.DispatcherEntry;
 import com.janeirodigital.shapetrees.test.fixtures.RequestMatchingFixtureDispatcher;
-import lombok.SneakyThrows;
 import okhttp3.mockwebserver.MockWebServer;
 import org.apache.jena.graph.Graph;
-import org.apache.jena.graph.GraphUtil;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,7 +30,8 @@ public class RemoteResourceTests extends BaseShapeTreeTest {
         dispatcher = new RequestMatchingFixtureDispatcher(List.of(
                 new DispatcherEntry(List.of("remoteResource/resource-no-link-headers"), "GET", "/static/resource/resource-no-link-headers", null),
                 new DispatcherEntry(List.of("remoteResource/resource-empty-link-header"), "GET", "/static/resource/resource-empty-link-header", null),
-                new DispatcherEntry(List.of("remoteResource/resource-container-link-header"), "GET", "/static/resource/resource-container-link-header", null)
+                new DispatcherEntry(List.of("remoteResource/resource-container-link-header"), "GET", "/static/resource/resource-container-link-header", null),
+                new DispatcherEntry(List.of("errors/404"), "GET", "/static/resource/notpresent", null)
         ));
     }
 
@@ -176,47 +173,14 @@ public class RemoteResourceTests extends BaseShapeTreeTest {
         assertNotNull(resource.getLinkHeaders());
     }
 
-
-    /*
     @Test
-    @Order(1)
-    @SneakyThrows
-    @DisplayName("RemoteResource - Create Resource")
-    void createResource() {
-        putContent(new URI(ROOT_PATH+"testing.ttl"), false, "target/test-classes/test-data/apps/gh-deep/ericprud-user.ttl", null);
-        RemoteResource resource = new RemoteResource(new URI(ROOT_PATH+"testing.ttl"), AUTH_HEADER_VALUE);
-        assertEquals(resource.exists(), true);
+    void test404Target() throws URISyntaxException, IOException {
+        MockWebServer server = new MockWebServer();
+        server.setDispatcher(dispatcher);
+        RemoteResource resource = new RemoteResource(getURI(server, "/static/resource/notpresent"), null);
+        assertNull(resource.getGraph(getURI(server, "/static/resource/notpresent")));
+        assertNull(resource.getBody());
     }
 
-    @Test
-    @Order(2)
-    @SneakyThrows
-    @DisplayName("RemoteResource - UpdateGraph with refresh")
-    void updateResourceWithRefresh() {
-        RemoteResource resource = new RemoteResource(new URI(ROOT_PATH+"testing.ttl"), AUTH_HEADER_VALUE);
-        Graph existingGraph = resource.getGraph(new URI(ROOT_PATH+"testing.ttl"));
-        List<Triple> triplesToAdd = new ArrayList<>();
-        triplesToAdd.add(new Triple(NodeFactory.createURI(resource.getURI().toString()), NodeFactory.createURI("http://example.com/predicate"), NodeFactory.createLiteral("testing1")));
-        GraphUtil.add(existingGraph, triplesToAdd);
-
-        resource.updateGraph(existingGraph, true, AUTH_HEADER_VALUE);
-        assertEquals(true, resource.getBody().contains("testing1"));
-    }
-
-    @Test
-    @Order(3)
-    @SneakyThrows
-    @DisplayName("RemoteResource - UpdateGraph without refresh")
-    void updateResourceWithoutRefresh() {
-        RemoteResource resource = new RemoteResource(new URI(ROOT_PATH+"testing.ttl"), AUTH_HEADER_VALUE);
-        Graph existingGraph = resource.getGraph(new URI(ROOT_PATH+"testing.ttl"));
-        List<Triple> triplesToAdd = new ArrayList<>();
-        triplesToAdd.add(new Triple(NodeFactory.createURI(resource.getURI().toString()), NodeFactory.createURI("http://example.com/predicate"), NodeFactory.createLiteral("testing2")));
-        GraphUtil.add(existingGraph, triplesToAdd);
-
-        resource.updateGraph(existingGraph, false, AUTH_HEADER_VALUE);
-        assertEquals(true, resource.getBody().contains("testing2"));
-    }
-*/
 
 }
