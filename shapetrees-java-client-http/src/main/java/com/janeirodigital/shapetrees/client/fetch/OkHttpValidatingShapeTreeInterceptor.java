2edfeeaae55e0345b8267e0dbb1e2a92fa92cbee
Eric Prud'hommeaux <eric+github@w3.org>
2021-08-11 08:19:04 +0200

    ~ put OkHttpShapeTreeRequest in OkHttpValidatingShapeTreeInterceptor

--- shapetrees-java-client-http/src/main/java/com/janeirodigital/shapetrees/client/fetch/OkHttpValidatingShapeTreeInterceptor.java
+++ shapetrees-java-client-http/src/main/java/com/janeirodigital/shapetrees/client/fetch/OkHttpValidatingShapeTreeInterceptor.java
@@ -108,4 +108,74 @@ public class OkHttpValidatingShapeTreeInterceptor implements Interceptor {
 
         return builder.build();
     }
+
+    private class FetchShapeTreeRequest implements ShapeTreeRequest {
+        private final Request request;
+        private ShapeTreeResourceType resourceType;
+
+        public FetchShapeTreeRequest(Request request) {
+            this.request = request;
+        }
+
+        @Override
+        public String getMethod() {
+            return this.request.method();
+        }
+
+        @Override
+        public URI getURI() {
+            return this.request.url().uri();
+        }
+
+        @Override
+        public Map<String, List<String>> getHeaders() {
+            return this.request.headers().toMultimap();
+        }
+
+        @Override
+        public Map<String, List<String>> getLinkHeaders() {
+            return HttpHeaderHelper.parseLinkHeadersToMap(this.getHeaderValues(HttpHeaders.LINK.getValue()));
+        }
+
+        @Override
+        public List<String> getHeaderValues(String header) {
+            return this.request.headers(header);
+        }
+
+        @Override
+        public String getHeaderValue(String header) {
+            return this.request.header(header);
+        }
+
+        @Override
+        public String getContentType() {
+            if (this.getHeaders().containsKey(HttpHeaders.CONTENT_TYPE.getValue())) {
+                return this.getHeaders().get(HttpHeaders.CONTENT_TYPE.getValue()).stream().findFirst().orElse(null);
+            }
+            return null;
+        }
+
+        @Override
+        public ShapeTreeResourceType getResourceType() {
+            return this.resourceType;
+        }
+
+        @Override
+        public void setResourceType(ShapeTreeResourceType resourceType) {
+            this.resourceType = resourceType;
+        }
+
+        @Override
+        public String getBody() {
+            try (Buffer buffer = new Buffer()) {
+                if (this.request.body() != null) {
+                    Objects.requireNonNull(this.request.body()).writeTo(buffer);
+                }
+                return buffer.readUtf8();
+            } catch (IOException | NullPointerException ex) {
+                log.error("Error writing body to string");
+                return null;
+            }
+        }
+    }
 }
