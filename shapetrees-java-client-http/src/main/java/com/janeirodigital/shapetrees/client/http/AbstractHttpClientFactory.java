9cebca863ea05f5acd548c1f02247d6797aa2347
Justin Bingham <justin.bingham@janeirodigital.com>
2021-08-13 13:16:42 -0400

    remove abstract constructor

--- shapetrees-java-client-http/src/main/java/com/janeirodigital/shapetrees/client/http/AbstractHttpClientFactory.java
+++ shapetrees-java-client-http/src/main/java/com/janeirodigital/shapetrees/client/http/AbstractHttpClientFactory.java
@@ -7,9 +7,4 @@ import lombok.Synchronized;
 public abstract class AbstractHttpClientFactory {
     @Setter(onMethod_={@Synchronized}) @Getter(onMethod_={@Synchronized})
     private static HttpClientFactory factory;
-
-    private AbstractHttpClientFactory() {
-        throw new IllegalStateException("AbstractHttpClientFactory is a utility class and is not meant to be instantiated on its own");
-    }
-
 }
