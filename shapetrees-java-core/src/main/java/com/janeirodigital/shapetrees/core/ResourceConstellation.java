cf7f9727e62826018bb77a964d3a61d7a325ec78
Eric Prud'hommeaux <eric+github@w3.org>
2021-10-10 10:18:52 +0200

    ~ isolate user and metadata resources

--- shapetrees-java-core/src/main/java/com/janeirodigital/shapetrees/core/ResourceConstellation.java
+++ shapetrees-java-core/src/main/java/com/janeirodigital/shapetrees/core/ResourceConstellation.java
@@ -7,59 +7,62 @@ import java.net.URI;
 import java.util.Optional;
 
 public class ResourceConstellation {
+    // access parameters
     protected ResourceAccessor _resourceAccessor;
     protected ShapeTreeContext _shapeTreeContext;
 
-    private URI _userResourceUri;
-    protected Optional<ShapeTreeResource> _userResource;
+    // components
+    private UserResource userResource = new UserResource();
+    private MetadataResource metadataResource = new MetadataResource();
 
-    private URI _metadataResourceUri;
-    protected Optional<ShapeTreeResource> _metadataResource;
-
-    protected boolean _isManaged;
+    // discovered attributes
     protected boolean _isMetadata;
 
     public ResourceConstellation(URI uri, ResourceAccessor resourceAccessor, ShapeTreeContext shapeTreeContext) throws ShapeTreeException {
-        this._userResourceUri = uri;
+        this.userResource.uri = uri;
         this._resourceAccessor = resourceAccessor;
         this._shapeTreeContext = shapeTreeContext;
         ShapeTreeResource res = resourceAccessor.getResource(shapeTreeContext, uri);
         if (res.isMetadata()) {
             this._isMetadata = true;
-            this._userResource = Optional.empty();
-            this._userResourceUri = null;
-            this._metadataResource = Optional.of(res);
-            this._metadataResourceUri = uri;
-            this._isManaged = true;
+            this.userResource.resource = Optional.empty();
+            this.userResource.uri = null;
+            this.metadataResource.resource = Optional.of(res);
+            this.metadataResource.uri = uri;
+            this.userResource._isManaged = true;
         } else {
             this._isMetadata = false;
-            this._userResource = Optional.of(res);
-            this._userResourceUri = uri;
-            this._metadataResource = Optional.empty();
-            this._metadataResourceUri = null;
-            this._isManaged = res.isManaged(); // TODO test !isManaged.
+            this.userResource.resource = Optional.of(res);
+            this.userResource.uri = uri;
+            this.metadataResource.resource = Optional.empty();
+            this.metadataResource.uri = null;
+            this.userResource._isManaged = res.isManaged(); // TODO test !isManaged.
         }
     }
-    public boolean isManaged() { return this._isManaged; }
+    public boolean isManaged() { return this.userResource._isManaged; }
     public boolean isMetadata() { return this._isMetadata; }
     public ShapeTreeContext getShapeTreeContext() { return this._shapeTreeContext; }
     public ShapeTreeResource getUserResource() throws ShapeTreeException {
-        if (this._userResource.isEmpty()) {
-            this._userResource = Optional.of(this._resourceAccessor.getResource(this._shapeTreeContext, this._userResourceUri));
+        if (this.userResource.resource.isEmpty()) {
+            this.userResource.resource = Optional.of(this._resourceAccessor.getResource(this._shapeTreeContext, this.userResource.uri));
         }
-        return this._userResource.get();
+        return this.userResource.resource.get();
     }
     public ShapeTreeResource getMetadataResource() throws ShapeTreeException {
-        if (this._metadataResource.isEmpty()) {
-            this._metadataResource = Optional.of(this._resourceAccessor.getResource(this._shapeTreeContext, this._metadataResourceUri));
+        if (this.metadataResource.resource.isEmpty()) {
+            this.metadataResource.resource = Optional.of(this._resourceAccessor.getResource(this._shapeTreeContext, this.metadataResource.uri));
         }
-        return this._metadataResource.get();
+        return this.metadataResource.resource.get();
     }
 
     public class UserResource {
+        private URI uri;
+        protected Optional<ShapeTreeResource> resource;
+        protected boolean _isManaged;
     }
 
     public class MetadataResource {
-
+        private URI uri;
+        protected Optional<ShapeTreeResource> resource;
     }
 }
