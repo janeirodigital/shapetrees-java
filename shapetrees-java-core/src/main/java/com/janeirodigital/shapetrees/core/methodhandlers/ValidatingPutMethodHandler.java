ce7c30ff797d961ad411e727d2c308671409b9dc
Eric Prud'hommeaux <eric+github@w3.org>
2021-10-10 10:24:05 +0200

    ~ s/UserResource/UserOwnedResource/

--- shapetrees-java-core/src/main/java/com/janeirodigital/shapetrees/core/methodhandlers/ValidatingPutMethodHandler.java
+++ shapetrees-java-core/src/main/java/com/janeirodigital/shapetrees/core/methodhandlers/ValidatingPutMethodHandler.java
@@ -4,7 +4,6 @@ import com.janeirodigital.shapetrees.core.*;
 import com.janeirodigital.shapetrees.core.exceptions.ShapeTreeException;
 import com.janeirodigital.shapetrees.core.models.ShapeTreeContext;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Optional;
 
@@ -23,7 +22,7 @@ public class ValidatingPutMethodHandler extends AbstractValidatingMethodHandler 
                 // Target resource is for shape tree metadata, manage shape trees to plant and/or unplant
                 return Optional.of(manageShapeTree(rc, shapeTreeRequest, rc.getMetadataResource()));
             } else {
-                ShapeTreeResource targetResource = rc.getUserResource();
+                ShapeTreeResource targetResource = rc.getUserOwnedResource();
                 if (targetResource.isExists()) {
                     // The target resource already exists
                     if (targetResource.isManaged()) {
