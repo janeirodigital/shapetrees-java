c51342b979d04c3abd0c06e4d3ec044abbaf9b8a
Justin Bingham <justin.bingham@janeirodigital.com>
2022-03-17 05:59:59 -0400

    added helper methods for working with shape tree references

--- shapetrees-java-core/src/main/java/com/janeirodigital/shapetrees/core/validation/ShapeTreeReference.java
+++ shapetrees-java-core/src/main/java/com/janeirodigital/shapetrees/core/validation/ShapeTreeReference.java
@@ -2,9 +2,14 @@ package com.janeirodigital.shapetrees.core.validation;
 
 import com.janeirodigital.shapetrees.core.exceptions.ShapeTreeException;
 import lombok.Getter;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.ResourceFactory;
 
 import java.net.URL;
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Getter
 public class ShapeTreeReference {
@@ -34,6 +39,35 @@ public class ShapeTreeReference {
 
     public boolean viaPredicate() {
         return predicate != null;
+    }
+
+    public static Property getPropertyFromReference(ShapeTreeReference reference) {
+        Property property = null;
+        if (reference.viaPredicate()) { property = ResourceFactory.createProperty(reference.getPredicate().toString()); }
+        if (reference.viaShapePath()) {
+            // TODO - this is a bit of a workaround to extract the target property from the shape path, given the
+            // TODO - current lack of a shape path parser in java. It is functionally equivalent to viaPredicate
+            Pattern pattern = Pattern.compile("@\\S+~(\\S*$)");
+            Matcher matcher = pattern.matcher(reference.getShapePath());
+            if (!matcher.matches()) return null;
+            String parsed = matcher.group(1);
+            if (parsed == null) return null;
+            property = ResourceFactory.createProperty(parsed);
+        }
+        return property;
+    }
+
+    public static ShapeTreeReference findChildReference(ShapeTree shapeTree, URL childUrl) throws ShapeTreeException {
+        try {
+            Iterator<ShapeTreeReference> iterator = shapeTree.getReferencedShapeTrees();
+            while (iterator.hasNext()) {
+                ShapeTreeReference reference = iterator.next();
+                if (reference.getReferenceUrl().equals(childUrl)) { return reference; }
+            }
+        } catch (ShapeTreeException ex) {
+            throw new ShapeTreeException(500, "Failed to lookup shape tree references for shape tree: " + shapeTree.getId());
+        }
+        return null;
     }
 
 }
